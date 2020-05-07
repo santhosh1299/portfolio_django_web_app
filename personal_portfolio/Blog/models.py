dc4ee7456184b4fcdadcf2e10de3baf6291d9a5c
Santhosh <santhoshmanoharan1299@gmail.com>
2020-05-07 06:18:28 +0530

    final

--- personal_portfolio/Blog/models.py
+++ personal_portfolio/Blog/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
 # Create your models here.
+class Blog(models.Model):
+    title=models.CharField(max_length=100)
+    description=models.CharField(max_length=1000)
+    date=models.DateField()
+    def __str__(self):
+        return self.title
--- personal_portfolio/personal_portfolio/urls.py
+++ personal_portfolio/personal_portfolio/urls.py
@@ -15,7 +15,16 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from django.conf.urls.static import static
+from django.conf import settings
+from Portfolio import views
+from django.urls import include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', views.home,name='home' ),
+    path('blog/',include('Blog.urls'))
+    ,
+
 ]
+urlpatterns +=static(settings.MEDIA_URL,document_root=settings.MEDIA_ROOT)
