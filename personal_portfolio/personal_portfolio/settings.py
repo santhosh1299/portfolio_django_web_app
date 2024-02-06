b6399ed600aa8e7edceb74f1576639bd4a2a397d
santhosh1299 <santhoshmanoharan1299@gmail.com>
2024-02-05 19:55:37 -0700

    EDA Heading updated

--- personal_portfolio/personal_portfolio/settings.py
+++ personal_portfolio/personal_portfolio/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = '^)irpev&lq-4kxmjgmrj0t+7a1&1f1x%yt=*)5)9%hs+0^da!i'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
-ALLOWED_HOSTS = ['santhoshmanoharan.pythonanywhere.com']
+ALLOWED_HOSTS = ['santhoshmanoharan.pythonanywhere.com','127.0.0.1']
 
 
 # Application definition
